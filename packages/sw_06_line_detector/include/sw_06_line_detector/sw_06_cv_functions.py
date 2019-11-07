3ecaa4af45b6484017d68fd539abb8e24465a65c
tobicarvalho <tobi.carvalho@gmail.com>
2019-11-06 19:21:09 -0500

    my technique

--- packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
+++ packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
@@ -7,15 +7,15 @@ from numpy.lib.stride_tricks import as_strided
 
 ## CATEGORY 1
 def inRange(hsv_image, low_range, high_range):
-	# output=np.ones((hsv_image.shape[0],hsv_image.shape[1]), dtype=np.uint8)
-	# for i in range(3):
-	# 	output &= ((hsv_image[:,:,i]>low_range[i]) & (hsv_image[:,:,i]<high_range[i]))
-	# return output
-	return cv2.inRange(hsv_image, low_range, high_range)
+	output=np.ones((hsv_image.shape[0],hsv_image.shape[1]), dtype=np.uint8)
+	for i in range(3):
+		output &= ((hsv_image[:,:,i]>low_range[i]) & (hsv_image[:,:,i]<high_range[i]))
+	return output
+	#return cv2.inRange(hsv_image, low_range, high_range)
 
 def bitwise_or(bitwise1, bitwise2):
-	# return bitwise1 | bitwise2
-	return cv2.bitwise_or(bitwise1, bitwise2)
+	return bitwise1 | bitwise2
+	#return cv2.bitwise_or(bitwise1, bitwise2)
 
 def bitwise_and(bitwise1, bitwise2):
 	# return bitwise1 & bitwise2
@@ -23,51 +23,51 @@ def bitwise_and(bitwise1, bitwise2):
 	return cv2.bitwise_and(bitwise1, bitwise2)
 
 def getStructuringElement(shape, size):
-	# Assuming we always want a ellipse kernel
+	Assuming we always want a ellipse kernel
 
-	# b=int(size[0]/2)
-	# a=int(size[1]/2)
-	# kernel=np.zeros(size,dtype=np.uint8)
+	b=int(size[0]/2)
+	a=int(size[1]/2)
+	kernel=np.zeros(size,dtype=np.uint8)
 
-	# for i in range(size[0]):
-	# 	dy=i-b
-	# 	dx=int(round(a*np.sqrt((1-dy**2/b**2))))
-	# 	left=a-dx
-	# 	right=a+dx+1
-	# 	kernel[i,left:right]=1
+	for i in range(size[0]):
+		dy=i-b
+		dx=int(round(a*np.sqrt((1-dy**2/b**2))))
+		left=a-dx
+		right=a+dx+1
+		kernel[i,left:right]=1
 
-	# return kernel
+	return kernel
 
-	return cv2.getStructuringElement(shape, size)
+	# return cv2.getStructuringElement(shape, size)
 
 def dilate(bitwise, kernel):
 
-	# padding=(kernel.shape[0]-1)/2 # Assuming the kernel is odd
+	padding=(kernel.shape[0]-1)/2 # Assuming the kernel is odd
 	
-	# bitwise=np.pad(bitwise,padding, 'constant')
+	bitwise=np.pad(bitwise,padding, 'constant')
 
-	# bitwise = as_strided(
-	#     bitwise,
-	#     shape=(
-	# 	bitwise.shape[0] - kernel.shape[0] + 1,  # The feature map is a few pixels smaller than the input
-	# 	bitwise.shape[1] - kernel.shape[1] + 1,
-	# 	kernel.shape[0],
-	# 	kernel.shape[1],
-	#     ),
-	#     strides=(
-	# 	bitwise.strides[0],
-	# 	bitwise.strides[1],
-	# 	bitwise.strides[0],  # When we move one step in the 3rd dimension, we should move one step in the original data too
-	# 	bitwise.strides[1],
-	#     ),
-	#     writeable=False,  # totally use this to avoid writing to memory in weird places
-	# )
+	bitwise = as_strided(
+	    bitwise,
+	    shape=(
+		bitwise.shape[0] - kernel.shape[0] + 1,  # The feature map is a few pixels smaller than the input
+		bitwise.shape[1] - kernel.shape[1] + 1,
+		kernel.shape[0],
+		kernel.shape[1],
+	    ),
+	    strides=(
+		bitwise.strides[0],
+		bitwise.strides[1],
+		bitwise.strides[0],  # When we move one step in the 3rd dimension, we should move one step in the original data too
+		bitwise.strides[1],
+	    ),
+	    writeable=False,  # totally use this to avoid writing to memory in weird places
+	)
 
-	# bitwise=(bitwise*kernel).max(axis=(2,3))
+	bitwise=(bitwise*kernel).max(axis=(2,3))
 
-	# return bitwise
+	return bitwise
 
-	return cv2.dilate(bitwise, kernel)
+	# return cv2.dilate(bitwise, kernel)
 
 
 
